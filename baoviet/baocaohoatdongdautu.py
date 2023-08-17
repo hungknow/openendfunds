d1f190bc2a6b94db8e51827aa901f0c42fab4032
hungknow <hungknow@gmail.com>
2023-08-17 11:12:05 +0700

    baoveit: extract fromdate, todate

--- baoviet/baocaohoatdongdautu.py
+++ baoviet/baocaohoatdongdautu.py
@@ -1,7 +1,19 @@
 import pandas as pd
+import datetime
+
+class BaoVietBaoCaoHoatDongDauTu:
+    def __init__(self, fromDate, toDate):
+        self.fromDate = fromDate
+        self.toDate = toDate
 
 # Read the content of Excel file
 
 def read_BaoCaoHoatDongDauTu(excelFilePath):
-    excel_data_df = pd.read_excel(excelFilePath, index_col=0)
-    print('Excel Sheet to JSON:', excel_data_df.to_json(orient='records'))
+    excel_data_df = pd.read_excel(excelFilePath, sheet_name=None, header=None)
+    firstSheet = excel_data_df['Tong quat']
+    fromDate = datetime.date(firstSheet[3][4], firstSheet[3][3], 1)
+    toDate = datetime.date(firstSheet[3][4], firstSheet[3][3], 31)
+    # print(fromDate)
+
+    # print('Excel Sheet to JSON:', excel_data_df.to_json(orient='records'))
+    return firstSheet
